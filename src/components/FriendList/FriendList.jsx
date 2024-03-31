2948699418c3ffa71dc9135d257e7021ddea6307
Hennadii Polupan <146132514+genvarius79@users.noreply.github.com>
2024-04-01 00:17:39 +0100

    enable prettier

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,20 +1,19 @@
-import FriendListItem from "../FriendListItem/FriendListItem"
-import css from "./FriendList.module.css"
-export default function FriendList({friends}) {
-
-    return <ul className={css.list}>
-        
-			{friends.map((friends) => {
-                return <li
-                    className={css.items}
-                    key={friends.id}>
-                    <FriendListItem
-                        avatar={friends.avatar}
-                        name={friends.name}
-                        isOnline={friends.isOnline}
-                    />
-                </li>;
-			})}
-		</ul>
-
+import FriendListItem from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+export default function FriendList({ friends }) {
+  return (
+    <ul className={css.list}>
+      {friends.map(friends => {
+        return (
+          <li className={css.items} key={friends.id}>
+            <FriendListItem
+              avatar={friends.avatar}
+              name={friends.name}
+              isOnline={friends.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,30 +1,30 @@
-import clsx from "clsx";
-import css from "./TransactionHistory.module.css"
-export default function TransactionHistory({items}) {
-    
-    return <table className={css.table}>
-   <thead>
-    <tr>
-      <th className={css.tdHeader}>Type</th>
-      <th className={css.tdHeader}>Amount</th>
-      <th className={css.tdHeader}>Currency</th>
-    </tr>
-  </thead>
-    <tbody>
-  
-        
-        {items.map((items,idx) => {
-            const amountClasses = clsx(css.td, idx % 2 === 0 ? css.tdLight : css.tdDark)
-            return <tr key={items.id}>
-                    <td className={amountClasses}>{items.type}</td>
-                    <td className={amountClasses}>{items.amount}</td>
-                    <td className={amountClasses}>{items.currency}</td>
-                  </tr>
-  
+import clsx from 'clsx';
+import css from './TransactionHistory.module.css';
+export default function TransactionHistory({ items }) {
+  return (
+    <table className={css.table}>
+      <thead>
+        <tr>
+          <th className={css.tdHeader}>Type</th>
+          <th className={css.tdHeader}>Amount</th>
+          <th className={css.tdHeader}>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((items, idx) => {
+          const amountClasses = clsx(
+            css.td,
+            idx % 2 === 0 ? css.tdLight : css.tdDark
+          );
+          return (
+            <tr key={items.id}>
+              <td className={amountClasses}>{items.type}</td>
+              <td className={amountClasses}>{items.amount}</td>
+              <td className={amountClasses}>{items.currency}</td>
+            </tr>
+          );
         })}
-    </tbody>
-</table>
-
-        
-
+      </tbody>
+    </table>
+  );
 }
